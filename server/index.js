b71c5372926205f3a1700910d4c621c3a4e44086
VirenderRana <47326801+VirenderRana@users.noreply.github.com>
2022-03-08 01:10:10 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://virender:virendeR@123@cluster0.ydwrv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://<username>:<password>@cluster0.ydwrv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
